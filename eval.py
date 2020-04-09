3307574eae20b568344dc7ad2a3fd1615427d0af
Ayush Kumar <kr.ayush998@gmail.com>
2020-04-09 23:23:22 +0530

    Made minor changes in eval.py

--- eval.py
+++ eval.py
@@ -4,14 +4,13 @@ from PIL import Image, ImageDraw
 from losses import *
 from constants import *
 
-def eval2():
+def eval2(id):
     # check how well the keypoint regression model performs
     x_cropped = np.load("data/x_cropped.npy")
     y_keypts = np.load("data/y_keypts.npy")
     model = keras.models.load_model("models/saved-model-11-0.00.h5")
     # note that in this case the indices of x_cropped may not correspond to the image id
     # since some images contain two shoes for which we have two datapoints
-    id = 11000
     img = x_cropped[id]
     y_keypts_pred = model.predict(img.reshape(1, img_size_cropped, img_size_cropped, 3))[0]
     img = Image.fromarray(np.uint8(img * 255.0))
@@ -25,11 +24,10 @@ def eval2():
     img.save("test.png")
 
 
-def eval1():
+def eval1(img_id):
     # test how well the bounding box model performs
     y = np.load("data/y.npy")
     model = keras.models.load_model("models/saved-model-11-0.69.h5", custom_objects={'myLoss': myLoss})
-    img_id = 11000
     test_img_path = image_paths[img_id]
     img = Image.open(test_img_path)
     img_ = np.array(img.convert('RGB').resize((img_size_red, img_size_red)), dtype=np.float16).reshape((1, img_size_red, img_size_red, 3)) / 255.0
@@ -52,5 +50,5 @@ def eval1():
     img.save("test.png")
 
 if __name__ == "__main__":
-    # eval1()
-    # eval2()
+    eval1(11000)
+    # eval2(11000)
